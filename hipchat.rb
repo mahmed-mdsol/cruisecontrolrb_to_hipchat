a6939868d4237750b74585fd983a0c66023af1b4
Malik Ahmed <maahmed@mdsol.com>
2012-07-16 10:57:41 -0400

    Made hipchat methods static and DRYed up the query a bit.

--- hipchat.rb
+++ hipchat.rb
@@ -1,15 +1,35 @@
 require 'httparty'
+require 'cgi'
 
 class Hipchat
   include HTTParty
-  
-  def hip_post message, color = nil
-    self.class.post("https://api.hipchat.com/v1/rooms/message?" + 
-      "auth_token=#{ENV["HIPCHAT_AUTH_TOKEN"]}" +
-      "&message=#{URI.escape(message)}" +
-      "&from=#{ENV["HIPCHAT_FROM"] || "cruise-control"}" +
-      "&room_id=#{ENV["HIPCHAT_ROOM_ID"]}" + 
-      "&color=#{color}")
+
+  class << self
+    # Defaults to use if options aren't provided
+    # Most of these are hipchat API defaults
+    DEFAULTS = {
+      :auth_token => ENV['HIPCHAT_AUTH_TOKEN'],
+      :room_id => ENV['HIPCHAT_ROOM_ID'],
+      :from => ENV['HIPCHAT_FROM'] || 'Cruise Control',
+      :message_format => 'html',
+      :notify => '1', # default is 0, but I like notifications
+      :color => 'yellow',
+      :format => 'json'
+    }
+
+    def hip_post(message, options = {})
+      self.post("https://api.hipchat.com/v1/rooms/message?" + query_parameters(options))
+    end
+
+    private
+
+    # Turn the hash into a query string
+    def query_parameters(options = {})
+      # Keep only accepted parameters and overwrite defaults if provided
+      options = DEFAULTS.merge(options.reject{|k, v| !DEFAULTS.include?(k)})
+      # Join the k=v pairs with & to make a query string.
+      options.collect{|key, value| "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"}.join("&")
+    end
   end
-    
+
 end
