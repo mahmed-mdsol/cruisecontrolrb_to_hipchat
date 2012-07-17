9cc524f06322d4fc403d2cb1eeb16bc519753b31
Malik Ahmed <maahmed@mdsol.com>
2012-07-17 15:40:42 -0400

    Added some comments

--- responder_callbacks/covcuke_notifier.rb
+++ responder_callbacks/covcuke_notifier.rb
@@ -21,10 +21,16 @@ class CovCukeNotifier < ResponderCallback
 
 	# Note: These are shamelessly tailored to our setup. Ideally, this should be more general, but you can always
 	# override this with your own easily. Ruby FTW.
+
+	# Return a string of all the specs and coverage failures captured
+	# TODO might need to break this up if there are too many failures. Otherwise we might get an exception and it'll just die
 	def specs_cov_failures(log)
 		log.scan(/rm -r coverage.+?(1\).+?Finished in .+?\n).+(^.+LOC$)/m).flatten.join("\n").strip
 	end
 
+	# Returns an array of failures captured
+	# A giant string would end up causing Net::HTTP to choke, so we're sending back an array to send in chunks.
+	# Might need to do the same for specs_cov_failures, too.
 	def cucumber_failures(log)
 		log.scan(/(\(::\) failed steps \(::\).+?)(Failing Scenarios:.+?Scenario:[^\n\r]+)\n\n/m).flatten
 	end
