f886a0add812b9512314d8e400c3ecd23600da81
Malik Ahmed <maahmed@mdsol.com>
2012-07-17 21:57:17 -0400

    Smurf Cruise Award!

---
+++ responder_callbacks/smurf_award.rb
@@ -0,0 +1,55 @@
+require 'net/http'
+require 'nokogiri'
+require 'yaml'
+require 'responder_callback'
+require 'hipchat'
+
+# TODO: A general achievements class with custom achievement subclasses
+class SmurfAward < ResponderCallback
+	ACHIEVEMENTS_FILE = 'achievements.yml'
+	SMURF_ACHIEVEMENT = '<img src="/images/smurf_icon.gif" alt="Smurf Cruise Award" title="Smurf Cruise Award" />'
+
+	def after_response(status_hash, responder)
+		if status_hash[:activity] =~ /Success/i
+			base_url = responder.ccrb.base_url.gsub(/ *https?:\/\//, '').gsub(/\/ *$/, '')
+			source = Net::HTTP.get(base_url, '/')
+			smurfed = Nokogiri::HTML.parse(source).css('.failed').length == 0
+			if smurfed
+				committers = source.scan(/committed by (.+?) &lt;/).flatten.uniq # get the committers who are a part of the smurf cruise!
+				achievements = read_achievements
+					puts achievements.inspect
+				committers.each do |committer|
+					committer = committer.strip
+					achievements[committer] ||= {}
+					achievements[committer][SMURF_ACHIEVEMENT] = true
+				end
+				write_achievements(achievements)
+				Hipchat.hip_post("<b>Achievement Unlocked: <i>Smurf Cruise!</i></b><br>+1 and achievement awarded to #{committers.join(', ')}!", :color => 'purple')
+			end
+		end
+	end
+
+	private
+
+	def read_achievements
+		self.class.read_achievements
+	end
+
+	def write_achievements(achievements)
+		self.class.write_achievements(achievements)
+	end
+
+	def self.read_achievements
+		if File.exists?(ACHIEVEMENTS_FILE)
+			return YAML::load(File.open(ACHIEVEMENTS_FILE, 'r'))
+		else
+			File.open(ACHIEVEMENTS_FILE, 'w'){|f| YAML::dump({})}
+			return {}
+		end
+	end
+
+	def self.write_achievements(achievements)
+		File.open(ACHIEVEMENTS_FILE, 'w'){|f| YAML::dump(achievements, f)}
+	end
+
+end
