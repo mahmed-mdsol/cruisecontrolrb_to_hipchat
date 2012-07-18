0c24a1320c89c823b7b6887b4e6a64e9ce037d76
Malik Ahmed <maahmed@mdsol.com>
2012-07-18 10:37:53 -0400

    %s/\t/  /g

--- run.rb
+++ run.rb
@@ -1,8 +1,8 @@
 # Silly little hack to use the config.ru file and start the app without needing rackup
 ENV['CAPN_CRUISE_PORT'] ||= "4567"
 def run(cls)
-	cls.run!(:port => ENV['CAPN_CRUISE_PORT'])
-	File.open('capn.pid', 'w'){|f| f.puts(Process.pid)}
+  cls.run!(:port => ENV['CAPN_CRUISE_PORT'])
+  File.open('capn.pid', 'w'){|f| f.puts(Process.pid)}
 end
 
 load './config.ru'
