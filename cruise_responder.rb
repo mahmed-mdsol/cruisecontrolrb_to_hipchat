3ee80331c9477b9515b32d84dc624cc6be0429b6
Malik Ahmed <maahmed@mdsol.com>
2012-07-17 21:54:50 -0400

    Only send non-empty messages.

--- cruise_responder.rb
+++ cruise_responder.rb
@@ -88,16 +88,18 @@ class CruiseResponderTask
 
 				# Interpolate the build_url into the response and the lastBuildStatus if needed
 				status_sym = new_activity.downcase.to_sym
-				message = (responses[status_sym] || 'Build status: %s') % [status_hash[:build_url], status_hash[:lastBuildStatus]]
+				message = (responses[status_sym] || '') % [status_hash[:build_url], status_hash[:lastBuildStatus]]
 				
-				# Callbacks
-				callbacks.each{|callback| callback.before_response(status_hash, self)}
+				unless message.empty?
+					# Callbacks
+					callbacks.each{|callback| callback.before_response(status_hash, self)}
 
-				Hipchat.hip_post(message, :color => colors[status_sym])
-				
-				@activity = new_activity
-				# Call callbacks
-				callbacks.each{|callback| callback.after_response(status_hash, self)}
+					Hipchat.hip_post(message, :color => colors[status_sym]) unless message.empty?
+
+					@activity = new_activity
+					# Call callbacks
+					callbacks.each{|callback| callback.after_response(status_hash, self)}
+				end
 			end
     end
 	end
