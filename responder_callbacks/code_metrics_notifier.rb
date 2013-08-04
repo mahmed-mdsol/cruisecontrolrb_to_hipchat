ee0509c6ee03755471cc959fc5cdbb4c21f77909
Gurunandan Pai <gpai@mdsol.com>
2013-08-03 21:05:01 -0400

    Change in url generation for code metrics artifacts

--- responder_callbacks/code_metrics_notifier.rb
+++ responder_callbacks/code_metrics_notifier.rb
@@ -11,7 +11,7 @@ class CodeMetricsNotifier < ResponderCallback
 	def after_response(status_hash, responder)
 		if responder.activity =~ /Success/i
 			build_url = URI.parse(status_hash[:build_url])
-			code_metrics_url = "#{build_url.path}/artifacts/code_metrics/html/index.html"
+      code_metrics_url = URI.join(build_url.to_s, 'artifacts/code_metrics/html/index.html')
       Hipchat.hip_post("<a href='#{code_metrics_url}'>Code Metrics for the build</a>", :color => 'green')
 		end
 	end
