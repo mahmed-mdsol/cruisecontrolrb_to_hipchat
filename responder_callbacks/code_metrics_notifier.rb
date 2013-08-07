265edbf808f18fd71113872a2117939247c6a87e
Malik Ahmed <maahmed@mdsol.com>
2013-08-07 10:58:12 -0400

    Fix whitespace wonkiness

--- responder_callbacks/code_metrics_notifier.rb
+++ responder_callbacks/code_metrics_notifier.rb
@@ -5,12 +5,12 @@ require 'hipchat'
 # Callback class that posts the code metrics of a successful build to hipchat
 class CodeMetricsNotifier < ResponderCallback
 
-	# On successful cruise build, send code metrics link to hipchat.
-	def after_response(status_hash, responder)
-		if responder.activity =~ /Success/i
+  # On successful cruise build, send code metrics link to hipchat.
+  def after_response(status_hash, responder)
+    if responder.activity =~ /Success/i
       code_metrics_url = "#{status_hash[:build_url]}/artifacts/code_metrics/html/index.html"
       Hipchat.hip_post("<a href='#{code_metrics_url}'>Code Metrics for the build</a>", :color => 'green')
-		end
-	end
+    end
+  end
 
 end
